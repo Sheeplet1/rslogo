f31b9b1360ca949f7633efe99d6e868ed2f6b782
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2024-03-09 13:24:35 +1100

    add: correct images and image generation script

---
+++ images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd logo_examples || exit
+
+for file in *.lg; do
+	prefix="$(echo "$file" | sed 's/\(.*\)\..*/\1/')"
+	cargo run "$file" "${prefix}_mine.svg" 500 500
+done
+
+mkdir images
+mv *.svg images
+cd - || exit
